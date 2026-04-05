830bf762c44aca730e6af91a649a09cc6e1ea693
Pranav Mallela <pranavmallela2024@gmail.com>
2026-04-05 17:40:35 -0400

    obstructed pedestrian updated. ready for test video

--- functional_tests/obstructed_pedestrian_detection.py
+++ functional_tests/obstructed_pedestrian_detection.py
@@ -9,13 +9,38 @@ class ObstructedPedestrianDetection(FunctionalTest):
         self.hsv_obj = None
         self.pedestrian_model = None
         self.state = 1  # Start in lane keeping state
+        self.white_mask = None
+        self.yellow_mask = None
 
     def detect_pedestrian(self):
-        pass
+        results = self.pedestrian_model(self.image)
+        py2 = 0
+        px1 = 0
 
-    def find_lane(self):
-        pass
-    
+        for results in results:
+            boxes = results.boxes.xyxy.tolist()
+            confidences = results.boxes.conf.tolist()
+            class_ids = results.boxes.cls.tolist()
+
+            for box, confidence, class_id in zip(boxes, confidences, class_ids):
+                # class 0 is person (built in) and adjust confidence as needed
+                px1, py1, px2, py2 = map(int, box)
+                if class_id == 0 and confidence > 0.7:
+                    height, width = img.shape[:2]
+                    range = int(width/100)
+                    size_person = (px2-px1)/width
+                    cv2.rectangle(img, (px1, py1), (px2, py2), (0, 255, 0), 2)
+                    cv2.waitKey(1)    
+                    if size_person > 0.12:
+                        # print("person within range")
+                        return True, (py2 + range), px1
+                    else:
+                        return False, (py2 + range), px1
+                    
+
+                
+        return False, py2, px1
+
     def calculate_waypoint(self, left_line, right_line):
         pass
 
@@ -25,23 +50,26 @@ class ObstructedPedestrianDetection(FunctionalTest):
         # State 3: lane keeping
 
         if self.state == 1:
-            left_line, right_line = self.find_lane()
-            self.waypoint = self.calculate_waypoint(left_line, right_line)
-            if self.detect_pedestrian():
+            self.waypoint = (self.width // 2, 40)
+            pedestrian_detected, py2, px1 = self.detect_pedestrian()
+            if pedestrian_detected:
                 self.state = 2
         elif self.state == 2:
-            if self.detect_pedestrian():
-                self.waypoint = (self.width // 2, self.height // 2)  # Stop
+            pedestrian_detected, py2, px1 = self.detect_pedestrian()
+            if pedestrian_detected:
+                self.waypoint = (self.width // 2, self.height)  # Stop
             else:
                 self.state = 3
         elif self.state == 3:
-            left_line, right_line = self.find_lane()
-            self.waypoint = self.calculate_waypoint(left_line, right_line)
+            self.waypoint = (self.width // 2, 40)  # Continue lane keeping
 
         self.update_mask()
 
     def update_mask(self):
         mask = np.zeros((self.height, self.width), dtype=np.uint8)
+        mask, dict = self.hsv_obj.get_mask(self.image, mask)
+        self.white_mask = dict['white_mask']
+        self.yellow_mask = dict['yellow_mask']
         self.final_mask = mask
 
     def main(self):
