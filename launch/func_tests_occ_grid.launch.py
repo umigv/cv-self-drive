9bc8165168acba159b174d36a71fabbab3c7d101
Aditya Suri <aditya.as.suri@gmail.com>
2026-05-06 14:10:09 -0500

    Fix typo

--- launch/func_tests_occ_grid.launch.py
+++ launch/func_tests_occ_grid.launch.py
@@ -5,7 +5,7 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package="cv_self_drive",
-            executable="func_tests_occ_grid_node",
+            executable="func_tests_occ_grid",
             name="self_drive_node",
             output="screen",
             parameters=[
